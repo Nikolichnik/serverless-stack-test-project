b7094e60380fcf110c1d4349c147fae848733a4d
Nikola Stojanovic <nstojanovic@s4hc.com>
2022-01-03 18:22:12 +0100

    Formatting.

--- stacks/index.js
+++ stacks/index.js
@@ -2,9 +2,9 @@ import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
 
 export default function main(app) {
-  const storageStack = new StorageStack(app, "storage");
+    const storageStack = new StorageStack(app, "storage");
 
-  new ApiStack(app, "api", {
-    table: storageStack.table,
-  });
+    new ApiStack(app, "api", {
+        table: storageStack.table,
+    });
 }
